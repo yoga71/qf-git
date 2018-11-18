f5faac939b3445a8c5133dd2a5db186bdec04a2f
xiangyanjun <xiangyan.jun@qq.com>
2018-11-18 17:02:09 +0800

    Initial commit

---
+++ src/main/java/com/qf/qfgit/QfGitApplication.java
@@ -0,0 +1,12 @@
+package com.qf.qfgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QfGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QfGitApplication.class, args);
+	}
+}
---
+++ src/main/java/com/qf/qfgit/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.qf.qfgit.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+@RequestMapping(value = "git")
+public class Controller {
+    @RequestMapping(value = "hello")
+    public String hello(){
+        System.out.println("yoga71");
+        return "hello";
+    }
+}
